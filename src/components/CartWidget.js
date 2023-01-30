a505a2c7554c5d946f34f04f65b9f85702323507
edgar4all <edgardo.alemis@excelsos.com.ar>
2023-01-30 13:26:42 -0300

    Ecommerce NavBar - Primera pre-entrega
    Ecommerce: NavBar - Primera pre-entrega

---
+++ src/components/CartWidget.js
@@ -0,0 +1,10 @@
+import React from "react";
+
+const CartWidget = () => {
+    return <>    
+        <i className="fa fa-shopping-cart"></i>
+        &nbsp; 0    
+    </>
+}
+
+export default CartWidget;
